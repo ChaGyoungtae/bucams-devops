16d61d81448ab72d1dbe9ed71be33fa78d0d08b8
ChaGyoungtae <village1031@gmail.com>
2025-04-11 12:39:30 +0900

    젠킨스 시작

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
-  timeout: 1000,
+  baseURL: 'https://bucams-api.com:30804/api/v1',
+  timeout: 2000,
 });
 
 apiClient.interceptors.request.use(
